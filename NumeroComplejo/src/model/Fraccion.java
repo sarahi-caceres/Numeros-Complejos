d7a54015ed287aa45cc466d8401979e141a45a14
sarahi-caceres <sara.c.v330241@gmail.com>
2026-08-11 13:32:31 -0400

    clase fraccion

---
+++ NumeroComplejo/src/model/Fraccion.java
@@ -0,0 +1,19 @@
+package model;
+
+public class Fraccion {
+    private int numerador;
+    private int denominador;
+    
+    public Fraccion(int numerador, int denominador) {
+        if (denominador == 0) {
+           System.out.println("Advertencia: denominador no puede ser cero. Se asignó 1 por defecto.");
+           denominador = 1;
+        }
+        this.numerador = numerador;
+        this.denominador = denominador;
+    }
+
+    
+
+}
+
