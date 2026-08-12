a77ca54083380a363e8778a102d036a5b8fdc9b7
dnina297 <danielnina1@upb.edu>
2026-08-11 22:48:06 -0400

    nose donde estoy

--- NumeroComplejo/src/App.java
+++ NumeroComplejo/src/App.java
@@ -1,5 +1,11 @@
+import model.Fraccion;
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        Fraccion f1=new Fraccion(2, 5);
+        Fraccion f1=new Fraccion(2, 5);
+        f1.SacarMcm(8,72);
+        f1.SacarMcd(8,72);
     }
 }
--- NumeroComplejo/src/model/Fraccion.java
+++ NumeroComplejo/src/model/Fraccion.java
@@ -12,8 +12,39 @@ public class Fraccion {
         this.numerador = numerador;
         this.denominador = denominador;
     }
+    public Fraccion Sumar (Fraccion fraccion2){
+        Fraccion fraccionSuma(0,0);
 
-    
+        
+        return fraccionSuma;
+    }
+    public int SacarMcm(int den1, int den2){
+        int mcm=0;
+        mcm= den1*den2/ SacarMcd(den1, den2);
+        return mcm;
+
+    }
+    public int SacarMcd(int den1, int den2){
+        int mcd=0;
+        int temp=0;
+        int mayor=0;
+        int menor=0;
+        if( den1>=den2){
+            mayor=den1;
+            menor=den2;
+        }
+        else{
+            mayor=den2;
+            menor=den1;
+        }
+        while(menor!=0){
+            temp=menor;
+            menor = mayor % menor;
+            mayor=temp;
+
+        }
+        return mcd;
+    }
 
 }
 
